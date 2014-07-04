747c378990791167f692c4d9f1fd1ea6c7038e70
Chandra.Sengupta <sayan801@gmail.com>
2014-07-04 16:43:41 +0530

    adding code solving errors...

--- imageuploadapp/src/main/java/com/technicise/androiduploadimagetophpserver/imageuploadapp/AndroidUploadActivity.java
+++ imageuploadapp/src/main/java/com/technicise/androiduploadimagetophpserver/imageuploadapp/AndroidUploadActivity.java
@@ -5,15 +5,94 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class AndroidUploadActivity extends ActionBarActivity {
 
+    TextView textView1;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_upload);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        try {
+            upload();
+        } catch (Exception e) {
+            Toast toast = Toast.makeText(this, e.toString(), Toast.LENGTH_LONG);
+            toast.show();
+            e.printStackTrace();
+        }
     }
 
+    public void upload() throws Exception {
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
+
+        String pathToOurFile = "/sdcard/technicise/1.jpg";
+        String urlServer = "http://192.168.2.1/androidupload/index.php";
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        FileInputStream fileInputStream = new FileInputStream(new File(pathToOurFile));
+        URL url = new URL(urlServer);
+
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        connection.setUseCaches(false);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Connection", "Keep-Alive");
+        connection.setRequestProperty("Content-Type",
+                "multipart/form-data;boundary=" + boundary);
+
+        outputStream = new DataOutputStream(connection.getOutputStream());
+        outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        outputStream
+                .writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\""
+                        + pathToOurFile + "\"" + lineEnd);
+        outputStream.writeBytes(lineEnd);
+
+        bytesAvailable = fileInputStream.available();
+        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+        buffer = new byte[bufferSize];
+
+        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+        while (bytesRead > 0) {
+            outputStream.write(buffer, 0, bufferSize);
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        }
+
+        outputStream.writeBytes(lineEnd);
+        outputStream.writeBytes(twoHyphens + boundary + twoHyphens
+                + lineEnd);
+
+        String serverResponseMessage = connection.getResponseMessage();
+
+        textView1.setText(serverResponseMessage);
+
+        fileInputStream.close();
+        outputStream.flush();
+        outputStream.close();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
